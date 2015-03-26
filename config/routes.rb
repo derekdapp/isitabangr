81337bca164aa611afbd413be5bf808c61b75ed6
derekdapp <derekdapp@gmail.com>
2015-03-26 15:29:49 -0600

    home as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
