ae135361ee3baea6b1cba474abf019f767e59326
derekdapp <derekdapp@gmail.com>
2015-03-30 14:48:05 -0600

    basic searching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
 class PagesController < ApplicationController
   def home
+  	
+  	song = Echonest::Song.new('***REMOVED***')
+  	params = { results: 10, bucket: 'audio_summary',  artist: 'awolnation', title: 'guilty filthy soul', rank_type: 'familiarity' }
+    @results = song.search(params)
   end
+  private
+  protected
+
+  def is_bangr?(song)
+  	score = 0
+  	if song[:audio_summary][:energy] > 0.7
+  		score += 1
+  	end
+  	if song[:audio_summary][:tempo] > 130
+  		score += 1
+  	end
+  	if song[:audio_summary][:acousticness] < 0.083
+  		score += 1
+  	end
+  	if song[:audio_summary][:danceability] < 0.72
+  		score += 1
+  	end
+  	if score >= 3
+  		return true
+  	else
+  		return false
+  	end
+  end
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,22 @@
 module PagesHelper
+	def is_bangr?(song)
+  	score = 0
+  	if song[:audio_summary][:energy] > 0.7
+  		score += 1
+  	end
+  	if song[:audio_summary][:tempo] > 130
+  		score += 1
+  	end
+  	if song[:audio_summary][:acousticness] < 0.083
+  		score += 1
+  	end
+  	if song[:audio_summary][:danceability] < 0.72
+  		score += 1
+  	end
+  	if score >= 3
+  		return true
+  	else
+  		return false
+  	end
+  end
 end
