1a4add49ffdc5db662f8e877df03aa934d31fce9
derekdapp <derekdapp@gmail.com>
2015-04-09 14:02:18 -0600

    cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,9 @@
 class PagesController < ApplicationController
-  def home
-  	
-
-
-    
+  def home 
   end
 
   def search_echonest
-
     @results = RSpotify::Track.search(params[:query], limit: 10)
-    puts @results.first.artists.first.name
-
-  end
-
-  private
-  protected
-
-  def is_bangr?(song)
-  	score = 0
-  	if song[:audio_summary][:energy] > 0.7
-  		score += 1
-  	end
-  	if song[:audio_summary][:tempo] > 130
-  		score += 1
-  	end
-  	if song[:audio_summary][:acousticness] < 0.083
-  		score += 1
-  	end
-  	if song[:audio_summary][:danceability] < 0.72
-  		score += 1
-  	end
-  	if score >= 3
-  		return true
-  	else
-  		return false
-  	end
   end
 
 end
